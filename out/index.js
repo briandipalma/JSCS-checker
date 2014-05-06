4c0f4ac577f1ce5b85a181934f5ad3c200950ab0
Brian Di Palma <offler@gmail.com>
2014-05-06 23:46:20 +0100

    Refactor code to use ES6 modules.

--- out/index.js
+++ out/index.js
@@ -1,8 +1,41 @@
 "use strict";
+Object.defineProperties(exports, {
+  configureJSCS: {get: function() {
+      return configureJSCS;
+    }},
+  checkFileWithJSCS: {get: function() {
+      return checkFileWithJSCS;
+    }},
+  __esModule: {value: true}
+});
 var __moduleName = "./src/index";
-var sep = $traceurRuntime.assertObject(require("path")).sep;
 var Log = $traceurRuntime.assertObject(require("fell")).Log;
+var $__0 = $traceurRuntime.assertObject(require("path")),
+    sep = $__0.sep,
+    join = $__0.join;
 var Promise = require("bluebird");
 var Checker = require("jscs/lib/checker");
-console.info("Checker is", Checker);
-console.info("Promise is", Promise);
+var checker = new Checker();
+function configureJSCS() {
+  return new Promise(function(resolve, reject) {
+    try {
+      var jscsConfiguration = require(join(__dirname, "..", "conf", "jscs.json"));
+      checker.registerDefaultRules();
+      checker.configure(jscsConfiguration);
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+;
+function checkFileWithJSCS(filePath) {
+  checker.checkFile(filePath).then(afterJSCSCheck);
+}
+;
+function afterJSCSCheck(errors) {
+  errors.getErrorList().forEach(function(error) {
+    var explainedError = errors.explainError(error);
+    Log.error(explainedError);
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,57 +1,55 @@
-import {sep} from "path";
+/** @module JSCS-checker */
+
 import {Log} from "fell";
+import {sep, join} from "path";
 
 module Promise from "bluebird";
 module Checker from "jscs/lib/checker";
 
-//var checker = {};
+var checker = new Checker();
 
-console.info("Checker is", Checker);
-console.info("Promise is", Promise);
+/**
+ * Calling this will initialise and configure JSCS.
+ *
+ * @returns {Promise} A Promise that resolves once JSCS is created and configured.
+ */
+export function configureJSCS() {
+	return new Promise(function(resolve, reject) {
 
-///**
-// * Calling this will initialise and configure JSCS.
-// *
-// * @returns {Promise} A Promise that resolves once JSCS is created and configured.
-// */
-//exports.configureJSCS = function configureJSCS() {
-//	return new Promise(function(resolve, reject) {
-//
-//		try {
-//			var jscsConfiguration = require(path.join(__dirname, "..", "conf", "jscs.json"));
-//
-//			checker = new Checker();
-//
-//			checker.registerDefaultRules();
-//			checker.configure(jscsConfiguration);
-//
-//			resolve();
-//		} catch (error) {
-//			reject(error);
-//		}
-//	});
-//};
-//
-///**
-// * Once JSCS is configured calling this will style check the provided JS files.
-// *
-// * @param {string} filePath - Path of JS file to check with JSCS.
-// */
-//exports.checkFileWithJSCS = function checkFileWithJSCS(filePath) {
-//	checker.
-//	    checkFile(filePath).
-//	    then(afterJSCSCheck);
-//};
-//
-///**
-// * Called once a JS file has been processed by JSCS with a list of all styling errors within the file.
-// *
-// * @private
-// * @param {jscs/lib/Errors} errors - The JSCS Errors object which can be used to report on all style errors within the checked JS files.
-// */
-//function afterJSCSCheck(errors) {
-//	errors.getErrorList().forEach(function(error) {
-//		var explainedError = errors.explainError(error);
-//		Log.error(explainedError);
-//	});
-//}
+		try {
+			var jscsConfiguration = require(join(__dirname, "..", "conf", "jscs.json"));
+
+			checker.registerDefaultRules();
+			checker.configure(jscsConfiguration);
+
+			resolve();
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+/**
+ * Once JSCS is configured calling this will style check the provided JS files.
+ *
+ * @param {string} filePath - Path of JS file to check with JSCS.
+ */
+export function checkFileWithJSCS(filePath) {
+	checker.
+	checkFile(filePath).
+	then(afterJSCSCheck);
+};
+
+/**
+ * Called once a JS file has been processed by JSCS with a list of all styling errors within the file.
+ *
+ * @private
+ * @param {jscs/lib/Errors} errors - The JSCS Errors object which can be used to report on all style errors within the checked JS files.
+ */
+function afterJSCSCheck(errors) {
+	errors.getErrorList().forEach(function(error) {
+		var explainedError = errors.explainError(error);
+
+		Log.error(explainedError);
+	});
+}
